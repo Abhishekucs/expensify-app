5fdfc5925a5117ff4e6f8324fdc7c1cd7f9e6fd0
Abhishekucs <40858773+Abhishekucs@users.noreply.github.com>
2020-05-14 00:49:58 +0530

    Production Ready

--- src/app.js
+++ src/app.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import AppRouters from './routers/AppRouters';
 import store from './store/configStore'
 import { Provider } from 'react-redux'
-import { addExpense, editExpense, removeExpense } from './actions/expenses'
-import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters'
-import visibleExpenseData from './selectors/expense'
 import 'normalize.css/normalize.css'
 import './styles/style.scss'
 import 'react-dates/lib/css/_datepicker.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,46 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-    entry: "./src/app.js",
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [{
-            use: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    mode: 'development',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+module.exports = (env) => {
+    const isProduction = env === 'production'
+    const CSSExtract = new MiniCssExtractPlugin()
+
+    return {
+        entry: "./src/app.js",
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: "bundle.js"
+        },
+        module: {
+            rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                ]
+            }]
+        },
+        plugins: [CSSExtract],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
 }
