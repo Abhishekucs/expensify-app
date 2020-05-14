91bb803b061ce9f8775fe54dd2688ab5d0865e23
Abhishekucs <40858773+Abhishekucs@users.noreply.github.com>
2020-05-14 22:57:20 +0530

    some feature added

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
 
 const ExpenseListItem = ({ id, description, createdAt, amount }) => (
     <div>
         <Link to={`/edit/${id}`}><h1>{description}</h1></Link>
-        <p>{amount}-{createdAt}</p>
+        <p>
+            <span>Rs</span>{new Intl.NumberFormat('en-IN').format(amount)}
+            -
+            {moment(createdAt).format('MMM Do, YYYY')}
+        </p>
     </div>
 )
 
