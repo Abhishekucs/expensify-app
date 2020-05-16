f6606e5580751fa918b17df6af55455e3e777961
Abhishekucs <40858773+Abhishekucs@users.noreply.github.com>
2020-05-16 23:18:02 +0530

    small change

--- webpack.config.js
+++ webpack.config.js
@@ -49,14 +49,14 @@ module.exports = (env) => {
         plugins: [
             CSSExtract,
             new webpack.DefinePlugin({
-            'process.env.FIREBASE_API_KEY'= JSON.stringify(process.env.FIREBASE_API_KEY),
-            'process.env.FIREBASE_AUTH_DOMAIN'= JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-            'process.env.FIREBASE_DATABASE_URL'= JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-            'process.env.FIREBASE_PROJECT_ID'= JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-            'process.env.FIREBASE_STORAGE_BUCKET'= JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-            'process.env.FIREBASE_MESSAGING_SENDER_ID'= JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-            'process.env.FIREBASE_APP_ID'= JSON.stringify(process.env.FIREBASE_APP_ID),
-            'process.env.FIREBASE_MEASUREMENT_ID'= JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID)
+            'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+            'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+            'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+            'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+            'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+            'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+            'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+            'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID)
             })
         ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',
